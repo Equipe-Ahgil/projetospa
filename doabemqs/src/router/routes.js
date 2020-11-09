8c348b6bb4eb476162b2d3a33dc6b6f7d664b31e
Samuelikz <samuel.silva@a.ficr.edu.br>
2020-11-08 23:06:54 -0300

    'Alterações'

--- doabemqs/src/router/routes.js
+++ doabemqs/src/router/routes.js
@@ -5,10 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: 'casos', component: () => import('pages/Casos.vue') },
-      { path: 'cadpfisica', component: () => import('pages/CadPfisica.vue') },
-      { path: 'cadjuridica', component: () => import('pages/CadJuridica.vue') },
-      { path: 'cadbeneficiario', component: () => import('pages/CadBeneficiario.vue') }
+      { path: 'casos', component: () => import('pages/Casos.vue') }
     ]
   },
 
