b5a8e3d88a2e82edd652dd02ebcdd04c7ce3197f
irlaandrade <paula.irla@gmail.com>
2020-11-16 11:08:38 -0300

    Adição da pagina sobre e seus componentes

--- doabemqs/src/components/HeaderSobre.vue
+++ doabemqs/src/components/HeaderSobre.vue
@@ -2,18 +2,17 @@
   <div>
 
     <q-img
-      src="../assets/banner.svg"
+      src="../assets/banner-sobre.svg"
       style="height: 400px; max-width: 100%"
     >
 
-    <div class="descrincao-banner flex flex-center">
-      <div class="text-banner">
-        <p>O Maior Site de Doações do Brasil</p>
+    <div class="descricao-banner-sobre flex flex-center">
+      <div class="text-banner-sobre">
+        <p>Já somamos mais de 200 voluntários</p>
       </div>
     </div>
 
     </q-img>
-
   </div>
 </template>
 
@@ -25,3 +24,31 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+
+.descricao-banner-sobre
+  flex-direction: column
+
+</style>
+
+<style lang="stylus">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap');
+
+.q-img__content div {
+  position: relative;
+  padding: 16px;
+  background: rgba(0,0,0,0);
+}
+
+.text-banner-sobre p {
+  color: white;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 54px;
+  line-height: 300px;
+  text-align: center;
+}
+
+</style>
